71334f1a4104e04759f0348f52e31c177816e993
Anuchat(Intern-Top) kamka <anuchat_kam@true-e-logistics.com>
2021-03-02 17:19:34 +0700

    😱 updated Todolist

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   Space,
   Typography,
 } from "antd";
+import { useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -20,57 +21,94 @@ const Container = styled.div`
   padding: 16px 24px;
 `;
 
-function App() {
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const addTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        id: todos.length + 1,
+        text: todo,
+        completed: false,
+      },
+    ]);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (todo === "") return;
+    addTodo();
+    setTodo("");
+  };
+
+  const removeTodo = (todoId) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== todoId);
+    setTodos(updatedTodos);
+  };
+
+  //updatedTodos done
+  const toggleTodo = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      return todo.id === todoId
+        ? { ...todo, completed: !todo.completed }
+        : todo;
+    });
+    setTodos(updatedTodos);
+  };
+
   return (
     <Container>
-      <Space>
-        <Input style={{ width: 400 }} placeholder="Enter Task Name" />
-        <Button type="primary">Create Task</Button>
-      </Space>
-      <Space direction="vertical" style={{ marginTop: 24 }}>
-        <Card
-          title="Sample Task"
-          style={{ width: 600 }}
-          extra={
-            <>
-              <Button type="primary">Duplicate</Button>{" "}
-              <Button type="primary" danger>
-                Delete
-              </Button>
-            </>
-          }
-        >
-          <Space direction="vertical" style={{ width: "100%" }}>
-            <Space>
-              <Input placeholder="Enter Subtask Name" style={{ width: 400 }} />
-              <Button type="primary">Add Subtask</Button>
-            </Space>
-            <Divider />
-            <Row>
-              <Col span={16}>
-                <Typography.Text>Subtask Name (Todo)</Typography.Text>
-              </Col>
-              <Col span={8}>
-                <Button type="primary">Done</Button>{" "}
-                <Button type="danger">Delete</Button>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={16}>
-                <Typography.Text style={{ textDecoration: "line-through" }}>
-                  Subtask Name (Done)
-                </Typography.Text>
-              </Col>
-              <Col span={8}>
-                <Button type="primary">Undo</Button>{" "}
-                <Button type="danger">Delete</Button>
-              </Col>
-            </Row>
-          </Space>
-        </Card>
-      </Space>
+      <form onSubmit={onSubmit}>
+        <Space>
+          <Input
+            style={{ width: 400 }}
+            placeholder="Enter Task Name"
+            onChange={handleChange}
+            value={todo}
+          />
+          <button type="submit">Add</button>
+        </Space>
+      </form>
+      <br />
+      <div>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <br />
+            <Card
+              style={{ width: 600 }}
+              onClick={() => toggleTodo(todo.id)}
+              title={todo.text}
+              extra={
+                <button onClick={() => removeTodo(todo.id)}>Delete</button>
+              }
+            >
+              <Space direction="vertical" style={{ width: "100%" }}>
+                <Space>
+                  <Input
+                    placeholder="Enter Subtask Name"
+                    style={{ width: 400 }}
+                  />
+                  <Button type="primary">Add Subtask</Button>
+                </Space>
+                <Divider />
+                <Row>
+                  <Col span={16}></Col>
+                  <Col span={8}></Col>
+                </Row>
+              </Space>
+            </Card>
+            <button onClick={() => removeTodo(todo.id)}>Delete</button>
+          </div>
+        ))}
+      </div>
     </Container>
   );
-}
+};
 
 export default App;
